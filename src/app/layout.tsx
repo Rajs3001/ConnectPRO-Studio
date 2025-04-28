346d8fee4db508fac985e9d028e150753eeabe2d
Rajdeep Saha <rajdeepsaha626@gmail.com>
2025-04-28 13:36:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <head>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <ClientSegmentRoot Component={function RootLayout} slots={{...}} params={{}}>
    >                       <RootLayout params={Promise}>
    >                         <html lang="en" className="dark">
    > >                         <head>
    >                             <meta>
    >                             <meta>
    >                             <title>
    > >                           {" "}
    >                             ...
    >                           ...
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (79:8) @ RootLayout
    >
    >   77 |   return (
    >   78 |     <html lang="en" className="dark">
    > > 79 |        <head>{/* Removed whitespace here */}
    >      |        ^
    >   80 |          {/* Meta tags managed by Next.js metadata API in pages */}
    >   81 |          {/* Basic meta tags required in head */}
    >   82 |          <meta charSet="utf-8" />
    >
    > Call Stack
    > 15
    >
    > Show 13 ignore-listed frame(s)
    > head
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (79:8)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="dark">
-       <head>{/* Removed whitespace here */}
+       <head>
          {/* Meta tags managed by Next.js metadata API in pages */}
          {/* Basic meta tags required in head */}
          <meta charSet="utf-8" />
